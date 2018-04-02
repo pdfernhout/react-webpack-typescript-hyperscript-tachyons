9bc493eca71e3da4a9fb3437feb690aa14775aad
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-04-02 18:02:23 -0400

    Added some validation to example

--- src/components/Hello.ts
+++ src/components/Hello.ts
@@ -9,13 +9,26 @@ export class Hello extends React.Component<HelloProps, {name: string}> {
         name: "world"
     };
 
+    isNameTooShort() {
+        return this.state.name.length < 3;
+    }
+
+    isNameTooLong() {
+        return this.state.name.length > 7;
+    }
+
+    maybeWarn() {
+        if (this.isNameTooShort() || this.isNameTooLong()) { return ".b--orange"; }
+        return "";
+    }
+
     render() {
         return h("div", [
             h("h1", `HyperScript Hello from ${this.props.compiler} and ${this.props.framework}!`),
             h("div", [
                 h("label", [
                     "Please enter your name",
-                    h("input", {
+                    h("input.ml1.ba.bw1" + this.maybeWarn(), {
                         value: this.state.name,
                         onChange: (event: {target: HTMLInputElement}) => {
                             // console.log("onChange:", event.target.value);
@@ -23,6 +36,10 @@ export class Hello extends React.Component<HelloProps, {name: string}> {
                         }
                     }),
                 ]),
+            ]),
+            h("div.h1.ma2", [
+                this.isNameTooShort() && h("div.orange", "Your name is too short (< 3 characters)"),
+                this.isNameTooLong() && h("div.orange", "Your name is too long (> 7 characters)"),
             ]),
             h("div", [
                 "Hello, ",
