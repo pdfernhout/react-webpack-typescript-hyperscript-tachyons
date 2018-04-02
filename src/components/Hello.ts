bb2657e952c68a6fcd359a9cb4eb9a96a45a4613
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-04-02 16:58:55 -0400

    Fancier hello with input

--- src/components/Hello.ts
+++ src/components/Hello.ts
@@ -4,9 +4,34 @@ import * as h from "react-hyperscript";
 export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export class Hello extends React.Component<HelloProps, {}> {
+export class Hello extends React.Component<HelloProps, {name: string}> {
+    state = {
+        name: "world"
+    };
+
     render() {
-        return h("h1", `updated Hello from ${this.props.compiler} and ${this.props.framework}!`);
+        return h("div", [
+            h("h1", `HyperScript Hello from ${this.props.compiler} and ${this.props.framework}!`),
+            h("div", [
+                h("label", [
+                    "Please enter your name",
+                    h("input", {
+                        value: this.state.name,
+                        onChange: (event: {target: HTMLInputElement}) => {
+                            // console.log("onChange:", event.target.value);
+                            this.setState({name: event.target.value});
+                        }
+                    }),
+                ]),
+            ]),
+            h("div", [
+                "Hello, ",
+                h("span", {style: {color: "green"}}, this.state.name),
+                h("br"),
+                "Your name has",
+                h("strong", {style: {margin: "0.25rem"}}, this.state.name.length),
+                "characters in it."
+            ]),
+        ]);
     }
 }
