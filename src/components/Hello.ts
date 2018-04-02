1cf845df7b3e3ec3d2761de3cce67b3b40f1bbda
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-04-02 16:15:05 -0400

    use HyperScript for Hello component.

--- src/components/Hello.ts
+++ src/components/Hello.ts
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as h from "react-hyperscript";
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -6,6 +7,6 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return h("h1", `updated Hello from ${this.props.compiler} and ${this.props.framework}!`);
     }
 }
